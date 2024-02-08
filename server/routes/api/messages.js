a09c8ac52d55a106106fe0906f3b43eee95197af
aidanho1188 <aaron23657@gmail.com>
2024-02-07 21:00:30 -0500

    minor bug fix

---
+++ server/routes/api/messages.js
@@ -0,0 +1,123 @@
+const mongoose = require('mongoose');
+const {Router} = require('express');
+const Message = require('../../models/Messages');
+const router = Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const message = await Message.find({});
+    res.status(200).json({
+      success: true,
+      data: {
+        message,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error',
+      error,
+    });
+  }
+});
+
+// router.get('/message/:id', (req, res) => {
+//   const messageId = parseInt(req.params.id);
+//   db.query('SELECT * FROM messages WHERE id = ?', [messageId], (err, rows) => {
+//     if (err) {
+//       res.status(500).json({error: err.message});
+//       return;
+//     }
+//     if (rows.length === 0) {
+//       res.status(404).json({error: 'Message not found'});
+//       return;
+//     }
+//     res.json(rows[0]);
+//   });
+// });
+
+router.post('/createMessage', async (req, res) => {
+  try {
+    const {timestamp, userId, roomId, message} = req.body;
+    if (!timestamp || !userId || !roomId || !message) {
+      return res.status(400).json({
+        success: false,
+        message: 'Missing required fields',
+      });
+    }
+    const newMessage = new Message({
+      timestamp,
+      userId,
+      roomId,
+      message,
+    });
+    await newMessage.save();
+
+    res.status(201).json({
+      success: true,
+      data: {
+        message: newMessage,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error',
+      error,
+    });
+  }
+});
+
+// // Delete a message
+// router.delete('/:id', (req, res) => {
+//   const messageId = parseInt(req.params.id);
+//   db.query('DELETE FROM messages WHERE id = ?', [messageId], (err, result) => {
+//     if (err) {
+//       res.status(500).json({error: err.message});
+//       return;
+//     }
+//     if (result.affectedRows === 0) {
+//       res.status(404).json({error: 'Message not found'});
+//       return;
+//     }
+//     res.sendStatus(204);
+//   });
+// });
+
+router.delete('/deleteMessage', async (req, res) => {
+  try {
+    const {id} = req.body;
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: 'No message selected for deletion',
+      });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        success: false,
+        message: 'Invalid message id',
+      });
+    }
+
+    await Message.findOneAndDelete({_id: mongoose.Types.ObjectId(id)});
+
+    res.status(200).json({
+      success: true,
+      data: {
+        message: 'Message deleted.',
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error',
+      error,
+    });
+  }
+});
+
+module.exports = router;
