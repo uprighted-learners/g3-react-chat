e32ed2ee983b5f8e294643f987389842865e475f
aidanho1188 <aaron23657@gmail.com>
2024-02-08 13:31:21 -0500

    add user delete endpoint

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -3,7 +3,7 @@ const {Router} = require('express');
 const User = require('../../models/Users');
 const router = Router();
 
-router.patch('/updateUser', async (req, res) => {
+router.patch('/update', async (req, res) => {
   try {
     const {firstName, lastName, email, password} = req.body;
     const userId = req.user.id;
@@ -50,6 +50,35 @@ router.patch('/updateUser', async (req, res) => {
         lastName: user.lastName,
         email: user.email,
       },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error',
+      error,
+    });
+  }
+});
+
+router.delete('/delete', async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: 'User not found',
+      });
+    }
+
+    await user.remove();
+
+    return res.status(200).json({
+      success: true,
+      message: 'User deleted successfully',
     });
   } catch (error) {
     console.error(error);
