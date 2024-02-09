7425ca306f3090e660cf109b7d54f43fe60973b3
simpsbri <simpsbri@gmail.com>
2024-02-09 14:00:22 -0600

    Update to user.js to simplify patch/update to iterate over any changes passed.

--- readme.md
+++ readme.md
@@ -1,18 +1,22 @@
 # Our App
+
 This is a full-stack web application that allows users to create and join rooms, send and receive messages, and their user accounts. It is built using the following technologies:
 
 Node.js and Express for the server
 MongoDB for the database
 
 ## Getting Started
+
 To get started with MyApp, follow these steps:
 
 ## Clone the repository:
+
 -Install the dependencies:
 -Start the server:
 -Open a web browser and navigate to http://localhost:3000 to view the app.
 
 ## Endpoints
+
 MyApp includes the following endpoints:
 
 - /room
@@ -32,12 +36,15 @@ MyApp includes the following endpoints:
   POST /: Creates a new user.
 
 ## Contributing
+
 We welcome contributions to MyApp! To get started, please fork the repository and make your changes on a new branch. When you are ready, submit a pull request and we will review your changes.
 
 ## License
+
 MyApp is released under the MIT License. See the LICENSE file for details.
 
 ## Contact
+
 For any questions or issues, please contact us at .
 
-Acknowledgments
+## Acknowledgments
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -4,11 +4,11 @@ const User = require('../../models/Users');
 const router = Router();
 const SALT = 10;
 
-router.patch('/update', async (req, res) => {
+router.patch('/update', async (req, res) =>
   try {
-    const { id, firstName, lastName, email, password } = req.body;
+    const { id, ...updateFields } = req.body;
 
-    if (!firstName && !lastName && !email && !password) {
+    if (Object.keys(updateFields).length === 0) {
       return res.status(400).json({
         success: false,
         message: 'Missing required fields',
@@ -23,21 +23,12 @@ router.patch('/update', async (req, res) => {
       });
     }
 
-    // this is a good example for function exaction?
-    if (firstName) {
-      user.firstName = firstName;
-    }
+    Object.keys(updateFields).forEach((key) => {
+      user[key] = updateFields[key];
+    });
 
-    if (lastName) {
-      user.lastName = lastName;
-    }
-
-    if (email) {
-      user.email = email;
-    }
-
-    if (password) {
-      user.password = bcryptjs.hashSync(password, SALT);
+    if (updateFields.password) {
+      user.password = bcryptjs.hashSync(updateFields.password, SALT);
     }
 
     await user.save();
@@ -55,6 +46,57 @@ router.patch('/update', async (req, res) => {
     sendErrorResponse(error, res);
   }
 });
+// router.patch('/update', async (req, res) => {
+//   try {
+//     const { id, firstName, lastName, email, password } = req.body;
+
+//     if (!firstName && !lastName && !email && !password) {
+//       return res.status(400).json({
+//         success: false,
+//         message: 'Missing required fields',
+//       });
+//     }
+
+//     const user = await User.findById(id);
+//     if (!user) {
+//       return res.status(404).json({
+//         success: false,
+//         message: 'User not found',
+//       });
+//     }
+
+//     // this is a good example for function exaction?
+//     if (firstName) {
+//       user.firstName = firstName;
+//     }
+
+//     if (lastName) {
+//       user.lastName = lastName;
+//     }
+
+//     if (email) {
+//       user.email = email;
+//     }
+
+//     if (password) {
+//       user.password = bcryptjs.hashSync(password, SALT);
+//     }
+
+//     await user.save();
+
+//     return res.status(200).json({
+//       success: true,
+//       message: 'User updated successfully',
+//       data: {
+//         firstName: user.firstName,
+//         lastName: user.lastName,
+//         email: user.email,
+//       },
+//     });
+//   } catch (error) {
+//     sendErrorResponse(error, res);
+//   }
+// });
 
 router.delete('/delete', async (req, res) => {
   try {
