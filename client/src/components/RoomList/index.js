6365a9ffb57b603e0747b82ac986bd21e8847dd0
simpsbri <simpsbri@gmail.com>
2024-02-14 16:55:04 -0600

    update to RoomList index.js to correct typos and add useEffect fetch

--- client/src/components/RoomList/index.js
+++ client/src/components/RoomList/index.js
@@ -1,19 +1,30 @@
 import './style.css';
-import React, { useEffect, useState } from 'react
+import React, { useEffect, useState } from 'react';
 
 const RoomsList = () => {
   const [rooms, setRooms] = useState([]);
 
   useEffect(() => {
-    // Fetch rooms from the API
+    const fetchRooms = async () => {
+      try {
+        const response = await fetch('./server/routes/api/rooms');
+        const data = await response.json();
+        setRooms(data);
+      } catch (error) {
+        console.error('Error fetching rooms: ', error);
+      }
+    };
+    fetchRooms();
   }, []);
 
   return (
     <div>
       <h1>Rooms List</h1>
-        {rooms.map((room) => (
-          <button className="room" key={room._id}>{room.name}</button>
-        ))}
+      {rooms.map((room) => (
+        <button className='room' key={room._id}>
+          {room.name}
+        </button>
+      ))}
     </div>
   );
 };
