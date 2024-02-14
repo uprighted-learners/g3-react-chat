3cc5eb311833cbbf2d63e93c51e93fb60529ca71
simpsbri <simpsbri@gmail.com>
2024-02-14 16:49:29 -0600

    Update to RoomList index.js

--- client/src/components/RoomList/index.js
+++ client/src/components/RoomList/index.js
@@ -1,11 +1,21 @@
 import './style.css';
-function RoomList() {
+import React, { useEffect, useState } from 'react
+
+const RoomsList = () => {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    // Fetch rooms from the API
+  }, []);
+
   return (
-    <div className="room-list">
-      <button className="room">This is a room</button>
-      <button className="room">This is a another room</button>
+    <div>
+      <h1>Rooms List</h1>
+        {rooms.map((room) => (
+          <button className="room" key={room._id}>{room.name}</button>
+        ))}
     </div>
   );
-}
+};
 
-export default RoomList;
+export default RoomsList;
