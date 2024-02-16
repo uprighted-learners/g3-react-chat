356cd91c9c3be3fdb398c2c1d19261707bc2f6d0
aidanho1188 <aaron23657@gmail.com>
2024-02-16 01:37:08 -0500

    Successfully read and verify user login

--- client/src/components/Login/SignIn.js
+++ client/src/components/Login/SignIn.js
@@ -12,11 +12,29 @@ function SignInForm() {
     });
   };
 
-  const handleOnSubmit = (evt) => {
+  const handleOnSubmit = async (evt) => {
     evt.preventDefault();
 
-    const { email, password } = state;
-    alert(`You are login with email: ${email} and password: ${password}`);
+    const {email, password} = state;
+    try {
+      const response = await fetch('http://localhost:8080/userAuth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({email, password}),
+      });
+
+      const res = await response.json();
+      if (response.ok) {
+        alert(res.message);
+      } else {
+        alert(res.message);
+      }
+    } catch (error) {
+      console.log(error);
+      alert(error.message);
+    }
 
     for (const key in state) {
       setState({
@@ -27,36 +45,24 @@ function SignInForm() {
   };
 
   return (
-    <div className='form-container sign-in-container'>
+    <div className="form-container sign-in-container">
       <form onSubmit={handleOnSubmit}>
         <h1>Sign in</h1>
-        <div className='social-container'>
-          <a href='#' className='social'>
-            <i className='fab fa-facebook-f' />
+        <div className="social-container">
+          <a href="#" className="social">
+            <i className="fab fa-facebook-f" />
           </a>
-          <a href='#' className='social'>
-            <i className='fab fa-google-plus-g' />
+          <a href="#" className="social">
+            <i className="fab fa-google-plus-g" />
           </a>
-          <a href='#' className='social'>
-            <i className='fab fa-linkedin-in' />
+          <a href="#" className="social">
+            <i className="fab fa-linkedin-in" />
           </a>
         </div>
         <span>or use your account</span>
-        <input
-          type='email'
-          placeholder='Email'
-          name='email'
-          value={state.email}
-          onChange={handleChange}
-        />
-        <input
-          type='password'
-          name='password'
-          placeholder='Password'
-          value={state.password}
-          onChange={handleChange}
-        />
-        <a href='#'>Forgot your password?</a>
+        <input type="email" placeholder="Email" name="email" value={state.email} onChange={handleChange} />
+        <input type="password" name="password" placeholder="Password" value={state.password} onChange={handleChange} />
+        <a href="#">Forgot your password?</a>
         <button>Sign In</button>
       </form>
     </div>
