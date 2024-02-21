47d2acc9d6d234bceeaa382b72977ce49eed4f33
simpsbri <simpsbri@gmail.com>
2024-02-21 17:18:11 -0600

    removed header on login function that was redundent

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,29 +1,39 @@
-import './styles.css';
-import SignInForm from '../../components/Login/SignIn';
-import SignUpForm from '../../components/Login/SignUp';
+import './styles.css'
+import SignInForm from '../../components/Login/SignIn'
+import SignUpForm from '../../components/Login/SignUp'
 
 function Login(props) {
-  const containerClass = 'container ' + (props.type === 'signUp' ? 'right-panel-active' : '');
+  const containerClass =
+    'container ' + (props.type === 'signUp' ? 'right-panel-active' : '')
 
   return (
     <>
-      <h2>Sign in/up Form</h2>
-      <div className={containerClass} id="container">
+      <div className={containerClass} id='container'>
         <SignUpForm />
         <SignInForm />
-        <div className="overlay-container">
-          <div className="overlay">
-            <div className="overlay-panel overlay-left">
+        <div className='overlay-container'>
+          <div className='overlay'>
+            <div className='overlay-panel overlay-left'>
               <h1>Welcome Back!</h1>
-              <p>To keep connected with us please login with your personal info</p>
-              <button className="ghost" id="signIn" onClick={() => props.handleOnClick('signIn')}>
+              <p>
+                To keep connected with us please login with your personal info
+              </p>
+              <button
+                className='ghost'
+                id='signIn'
+                onClick={() => props.handleOnClick('signIn')}
+              >
                 Sign In
               </button>
             </div>
-            <div className="overlay-panel overlay-right">
+            <div className='overlay-panel overlay-right'>
               <h1>Hello, Friend!</h1>
               <p>Enter your personal details and start journey with us</p>
-              <button className="ghost " id="signUp" onClick={() => props.handleOnClick('signUp')}>
+              <button
+                className='ghost '
+                id='signUp'
+                onClick={() => props.handleOnClick('signUp')}
+              >
                 Sign Up
               </button>
             </div>
@@ -31,7 +41,7 @@ function Login(props) {
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default Login;
+export default Login
