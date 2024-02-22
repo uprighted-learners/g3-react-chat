e6c12530e42f641af34d31fc528d32742c9c63ee
aidanho1188 <aaron23657@gmail.com>
2024-02-21 19:00:18 -0500

    pushed lasted version

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -91,11 +91,10 @@ router.delete(`/delete/:messageId`, async (req, res) => {
   }
 });
 
-router.patch('/update', checkMissingFields('user, messageId', 'newMessage'), isAdmin, async (req, res) => {
+router.patch('/update', checkMissingFields('user', 'messageId', 'newMessage'), isAdmin, async (req, res) => {
   try {
     const {messageId, newMessage} = req.body;
-
-    const foundMessage = await Message.findOneAndUpdate({_id: messageId}, {message: newMessage}, {new: true});
+    const foundMessage = await Message.findOneAndUpdate({_id: messageId}, {message: newMessage});
 
     if (!foundMessage) {
       return res.status(404).json({
